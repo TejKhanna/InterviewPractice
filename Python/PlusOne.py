0ca23ccc772d7619e820565e68f7d731d4255fce
Tej Khanna <tk1939@nyu.edu>
2020-10-02 18:55:22 -0400

    added plus one

---
+++ Python/PlusOne.py
@@ -0,0 +1,11 @@
+'''
+
+QUESTION: Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+'''
+
+return list(str(int(''.join([str(x) for x in digits]))+1))
